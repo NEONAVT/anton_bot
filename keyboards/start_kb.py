20bdf287947e622caca64e99c09d7513650365a7
Клим <klim.klushin@gmail.com>
2025-08-25 13:02:01 +0300

    init

--- keyboards/start_kb.py
+++ keyboards/start_kb.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import WebAppInfo
 start_kb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="📲 Заказать обратный звонок", web_app=WebAppInfo(url="https://NEONAVT.github.io/anton_bot/request_callback.html"))],
+    [InlineKeyboardButton(text="📲 Заказать обратный звонок", web_app=WebAppInfo(url="https://NEONAVT.github.io/anton_bot/"))],
     [InlineKeyboardButton(text="🔧 Услуги", callback_data="services"),
      InlineKeyboardButton(text="👨‍🔧 О нас", callback_data="about")],
     [InlineKeyboardButton(text="🧾 Стоимость", callback_data="prices"),
